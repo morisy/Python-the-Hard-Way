989427b8e49ad169001776a5a8635860e898ac0f
Michael Morisy <morisy@gmail.com>
2011-11-03 17:41:53 -0400

    added another file.

---
+++ ex39.py
@@ -0,0 +1,7 @@
+ten_things = "Apples Oranges Crows Telephones Light Sugar"
+
+print "Wait, there's not 10 things in that list. Let's fix it."
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl",
+"Boy"]
