4582d41574c281d0f92b96ada16158f09154eef3
Michael Morisy <morisy@gmail.com>
2011-11-12 11:34:18 -0500

    First pass at the game.

--- ex43.py
+++ ex43.py
@@ -1,4 +1,51 @@
 from sys import exit
 from random import randint
 
+class Game(object):
+    def __init__(self, start):
+        self.deck = [
+                "Introduction",
+                 "Problem",
+                "Closing",
+                "Flow Chart",
+                "Buzzwords"
+        ]
 
+        self.resolve = 30
+        self.awake = 1
+        self.pee = 0
+        self.cash = 200
+        self.start = start
+
+    def play(self):
+        next = self.start
+
+        while True:
+            print "\n======================="
+            room = getattr(self, next)
+            next = room()
+
+    def death(self):
+        next = self.start
+
+        while True:
+            room = getattr(self, next)
+            next = room()
+
+    def print_status(self):
+        print "You're feeling mighty frisky!"
+
+    def lobby(self):
+        print "Here it is, what the long, hellish morning has lead up to:"
+        print "Your chance to pitch the buyer, and either make or break"
+        print "your company once and for all."
+        print "Let's take a look at your deck:"
+        print self.deck
+        print "Not bad, hopefully you can make the sale."
+        print "But how are you feeling? You'll need all the stamina you can"
+        print "muster to out last the intimidating executive in front of you."
+        self.print_status()
+        print "Well, hope that's good enough!"
+
+a_game = Game("lobby")
+a_game.play()
