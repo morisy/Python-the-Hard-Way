35ab7d09d0e4b292667c4d803a29b9521b41b45f
Michael Morisy <morisy@gmail.com>
2011-11-13 17:48:42 -0500

    A little more work

--- projects/ex47/tests/ex47_tests.py
+++ projects/ex47/tests/ex47_tests.py
@@ -1,11 +1,5 @@
 from nose.tools import *
-import ex47 
+import ex47.game Room 
 
-def setup():
-    print "SETUP!"
-
-def teardown():
-    print "TEAR DOWN!"
-
-def test_basic():
-    print "I RAN!"
+def test_room():
+    gold = Room("GoldRoom", """This room has gold in it you can grab. There's a door to the north""")
