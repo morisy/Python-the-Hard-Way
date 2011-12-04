d4c4f6e2db9d59bfd237529b1e8da80178777af1
Michael Morisy <morisy@gmail.com>
2011-12-04 00:37:41 -0500

    Working test

--- projects/ex49/tests/parser_tests.py
+++ projects/ex49/tests/parser_tests.py
@@ -5,7 +5,7 @@ def test_peek():
     word_list = [('noun', 'Dog'), ('verb', 'ran'), ('stop', 'to'), ('stop', 'the'), ('noun', 'cow')]
     assert_equal(parser.peek(word_list), 'noun')
 
-def teardown():
+def test_match():
     print "TEAR DOWN!"
 
 def test_basic():
